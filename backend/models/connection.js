eb14325ad1ccb61638f2306107431c272e60e5b0
Kesraoui Mohamed <92538732+KesraouiMohamed1991@users.noreply.github.com>
2023-12-21 19:50:52 +0100

    d

--- backend/models/connection.js
+++ backend/models/connection.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 
-// const connectionString = process.env.CONNECTION_STRING;
-const connectionString = 'mongodb+srv://kesraoui15003:harlembynight@cluster0.unytmwf.mongodb.net/MarseilleSips';
+const connectionString = process.env.CONNECTION_STRING;
 mongoose.connect(connectionString, { connectTimeoutMS: 2000 })
   .then(() => console.log('Database connected'))
   .catch(error => console.error(error));
