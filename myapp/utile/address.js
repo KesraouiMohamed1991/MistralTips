c55ace629f254124f7bac3f604d3a0f0f9c580df
Kesraoui Mohamed <92538732+KesraouiMohamed1991@users.noreply.github.com>
2023-12-21 10:39:36 +0100

    adding the vercel.json file

--- myapp/utile/address.js
+++ myapp/utile/address.js
@@ -1,1 +1,2 @@
-export const BACKEND_ADDRESS = 'http://10.20.2.92:3000';
+// export const BACKEND_ADDRESS = 'http://10.20.2.92:3000';
+export const BACKEND_ADDRESS = 'https://marseille-tip-s-3598.vercel.app/';
