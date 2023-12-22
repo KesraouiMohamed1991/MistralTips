1d77b34a5e05be322f8f926516ed3f24a41bc3f8
Kesraoui Mohamed <92538732+KesraouiMohamed1991@users.noreply.github.com>
2023-12-22 10:06:23 +0100

    fontfam

--- myapp/screens/HomeSub/BarPage.jsx
+++ myapp/screens/HomeSub/BarPage.jsx
@@ -6,120 +6,122 @@ import { colors } from '../../utile/colors';
 import { addFav } from '../../reducers/favoris';
 import { useDispatch } from 'react-redux';
 
-const Barpage = ({ route, navigation }) => {
+      const Barpage = ({ route, navigation }) => {
 
-  const [isFavorite, setIsFavorite] = useState(false);
+        const [isFavorite, setIsFavorite] = useState(false);
 
 
-  const dispatch = useDispatch();
+        const dispatch = useDispatch();
 
-  const { markerData } = route.params;
-  const barName = markerData.title;
+        const { markerData } = route.params;
+        const barName = markerData.title;
 
-  const [data, setData] = useState([]);
-  const [showDetails, setShowDetails] = useState(false);
-  const [isLoading, setIsLoading] = useState(true);
+        const [data, setData] = useState([]);
+        
+        const [showDetails, setShowDetails] = useState(false);
+        const [isLoading, setIsLoading] = useState(true);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const filteredData = await fetchBarsData();
-        const BarInfo = filteredData.filter((bar) => bar.name === barName);
-        setData(BarInfo);
-        setIsLoading(false)
-      } catch (error) {
-        console.error('Error fetching data in Barpage:', error);
-        setIsLoading(false);
-      }
-    };
+        useEffect(() => {
+          const fetchData = async () => {
+            try {
+              const filteredData = await fetchBarsData();
+              const BarInfo = filteredData.filter((bar) => bar.name === barName);
+              setData(BarInfo);
+              setIsLoading(false)
+            } catch (error) {
+              console.error('Error fetching data in Barpage:', error);
+              setIsLoading(false);
+            }
+          };
 
-    fetchData();
-  }, [barName]);
+          fetchData();
+        }, [barName]);
 
-  if (isLoading) {
-    return (
-      <View style={styles.loadingContainer}>
-        <ActivityIndicator size="large" color={colors.Radiance} />
-      </View>
-    );
-  }
+        if (isLoading) {
+          return (
+            <View style={styles.loadingContainer}>
+              <ActivityIndicator size="large" color={colors.Radiance} />
+            </View>
+          );
+        }
 
-  const {
-    adresse,
-    horaires,
-    image,
-    name,
-    note,
-    numero,
-    presentation,
-    type,
-  } = data[0];
+        const {
+          adresse,
+          horaires,
+          image,
+          name,
+          note,
+          numero,
+          presentation,
+          type,
+        } = data[0];
 
-  function goToBars() {
-    navigation.navigate('Carte');
-  }
+        function goToBars() {
+          navigation.navigate('Carte');
+        }
 
-  function handleDetails() {
-    setShowDetails(!showDetails);
-  }
+        function handleDetails() {
+          setShowDetails(!showDetails);
+        }
 
-  function handleAddFavoris() {
-      dispatch(addFav({ name, adresse, image }));
-    setIsFavorite(!isFavorite);
-      
-  }
-    
-    
-    
+        function handleAddFavoris() {
+            dispatch(addFav({ name, adresse, image }));
+          setIsFavorite(!isFavorite);
+            
+        }
+          
+          
+          
 
-    
+          
 
-  return (
-        <ScrollView style={styles.container}>
-        <Image source={{ uri: image }} style={styles.image} />
-        <TouchableOpacity style={styles.icon} onPress={goToBars}>
-        <FontAwesome style={{ padding: 10 }} name="arrow-left" size={25} color={colors.DeepBlue} />
-        </TouchableOpacity>
-        <View style={styles.textContainer}>
-        <View style={styles.barNote}>
-        {[...Array(Math.floor(note))].map((_, index) => (
-        <FontAwesome key={index} style={{ padding: 1 }} name="star" size={20} color={colors.GoldenYellow} />
-        ))}
-        </View>
-        <Text style={styles.barName}>{name}</Text>
-        <Text style={styles.baradresse}>{adresse}</Text>
-        <Text style={styles.barsNum}>{numero === 'true' || numero === 'false' ? 'Pas de numéro' : numero}</Text>
-        <Text style={styles.barsType}>Type: {type}</Text>
-        <Text style={styles.barspresent}>Présentation: {presentation}.</Text>
-        <View style={{ justifyContent: 'center', alignItems: 'center' }}>
+        return (
+              <ScrollView style={styles.container}>
+              <Image source={{ uri: image }} style={styles.image} />
+              <TouchableOpacity style={styles.icon} onPress={goToBars}>
+              <FontAwesome style={{ padding: 10 }} name="arrow-left" size={25} color={colors.DeepBlue} />
+              </TouchableOpacity>
+              <View style={styles.textContainer}>
+              <View style={styles.barNote}>
+              {[...Array(Math.floor(note))].map((_, index) => (
+              <FontAwesome key={index} style={{ padding: 1 }} name="star" size={20} color={colors.GoldenYellow} />
+              ))}
+              </View>
+              <Text style={styles.barName}>{name}</Text>
+              <Text style={styles.baradresse}>{adresse}</Text>
+              <Text style={styles.barsNum}>{numero === 'true' || numero === 'false' ? 'Pas de numéro' : numero}</Text>
+              <Text style={styles.barsType}>Type: {type}</Text>
+              <Text style={styles.barspresent}>Présentation: {presentation}.</Text>
+              <View style={{ justifyContent: 'center', alignItems: 'center' }}>
 
-        <TouchableOpacity style={{ paddingVertical: 10, fontSize: 16, fontWeight: 'bold', fontSize: 16, width:'100%', justifyContent:'center', alignItems:'flex-start' }} onPress={handleDetails}>
-        <Text style={{textAlign:'center'}}>{showDetails ? 'Cacher Horaires ↴' : 'Voir Horaires ↴'}</Text>
-        </TouchableOpacity>
+              <TouchableOpacity style={{ paddingVertical: 10, fontSize: 16, fontWeight: 'bold', fontSize: 16, width:'100%', justifyContent:'center', alignItems:'flex-start' }} onPress={handleDetails}>
+          
+              <Text style={{textAlign:'center',  fontFamily: 'BricolageGrotesque', }}>{showDetails ? 'Cacher Horaires ↴' : 'Voir Horaires ↴'}</Text>
+              </TouchableOpacity>
 
-        {showDetails && (
-          <View style={styles.horaires}>
-          {Object.entries(horaires).slice(0, -1).map(([day, hours]) => (
-          <Text key={day}>{`${day.charAt(0).toUpperCase() + day.slice(1)}: ${hours}`}</Text>
-          ))}
+              {showDetails && (
+                <View style={styles.horaires}>
+                {Object.entries(horaires).slice(0, -1).map(([day, hours]) => (
+                <Text key={day}>{`${day.charAt(0).toUpperCase() + day.slice(1)}: ${hours}`}</Text>
+                ))}
 
-        </View>
-        )}
+              </View>
+              )}
 
-        <TouchableOpacity style={styles.btn} onPress={handleAddFavoris}>
-        <FontAwesome
-        style={{ padding: 10 }}
-        name="heart"
-        size={20}
-        color={isFavorite ? colors.Radiance : colors.DeepBlue}
-        />
-        <Text style={styles.btnText}>Favoris</Text>
-        </TouchableOpacity>
-        </View>
-        </View>
-        </ScrollView>
-  );
-};
+              <TouchableOpacity style={styles.btn} onPress={handleAddFavoris}>
+              <FontAwesome
+              style={{ padding: 10 }}
+              name="heart"
+              size={20}
+              color={isFavorite ? colors.Radiance : colors.DeepBlue}
+              />
+              <Text style={styles.btnText}>Favoris</Text>
+              </TouchableOpacity>
+              </View>
+              </View>
+              </ScrollView>
+        );
+      };
 
         const styles = StyleSheet.create({
         container: {
